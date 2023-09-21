ccd5ef70d62cd2c50c75351b45c87dc6302bccb7
why-is-everything-taken <145640294+why-is-everything-taken@users.noreply.github.com>
2023-09-21 05:17:35 -0400

    Update buyservers.js

--- scripts/buyservers.js
+++ scripts/buyservers.js
@@ -1,11 +1,10 @@
 /** @param {NS} ns */
-
 export async function main(ns) {
 
   let servers = [];
   let maxServers = ns.getPurchasedServerLimit();
   var availableCash = ns.getServerMoneyAvailable('home');
-  var i = 0;
+  var allMax = false;
 
   while (true) {
     let pservs = new Set(['home']);
@@ -17,24 +16,32 @@ export async function main(ns) {
     if (servers.length < maxServers) {
       if (availableCash > ns.getPurchasedServerCost(16)) {
         ns.tprint('Buying a server with 16 GB of RAM for ' + ns.getPurchasedServerCost(16));
-        ns.purchaseServer('pserv-' + i, 16);
+        ns.purchaseServer('pserv-' + servers.length, 16);
       }
     }
 
-    while (servers.length == maxServers) {
-      //check if pserv-(i) can be upgraded. if we can, and we can afford it, upgrade the ram by one level
-      if (ns.getServerMaxRam(servers[i]) < (2 ** 20) && availableCash > ns.getPurchasedServerUpgradeCost(servers[i], ns.getServerMaxRam(servers[i]) * 2)) {
-        ns.upgradePurchasedServer(servers[i], (ns.getServerMaxRam(servers[i])) * 2)
-        ns.tprint('Upgrading ' + servers[i] + ' to ' + ns.getServerMaxRam(servers[i])+' GB of RAM');
-      }
-      if (i == 24) {
-        i = 0;
-      }
-      if (i < 24) {
-        i++;
-      }
-      await ns.sleep(1000);
+    //check if pserv-(i) can be upgraded. if we can, and we can afford it, upgrade the ram by one level
+    while (servers.length === maxServers) {
+      for (let i = 0; i < servers.length; i++) {
+        var servRam = ns.getServerMaxRam(servers[i]) * 2;
+        ns.print(servRam)
+        await ns.sleep(500)
+        if (ns.getServerMaxRam(servers[i]) < (2 ** 20) && availableCash >= ns.getPurchasedServerUpgradeCost(servers[i], servRam)) {
+          ns.upgradePurchasedServer(servers[i], (ns.getServerMaxRam(servers[i])) * 2);
+          ns.tprint('Upgrading ' + servers[i] + ' to ' + ns.getServerMaxRam(servers[i]) + ' GB of RAM');
+        } else {
+          for (let i = servers.length - 1; i > 0; i--) {
+            if (ns.getServerMaxRam(servers[(i)]) == (2 ** 20)) {
+              allMax = true;
+            } else if (ns.getServerMaxRam(servers[(i)]) != (2 ** 20)) {
+              allMax = false;
+            }
+          }
+          if (allMax == true) {
+            ns.exit();
+          }
+        }
+      } await ns.sleep(500);
     }
-    await ns.sleep(1000);
   }
 }
