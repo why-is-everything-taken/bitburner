bb2bf5175fd459d471b21138597fdc5c44f1ea99
why-is-everything-taken <145640294+why-is-everything-taken@users.noreply.github.com>
2023-09-21 07:12:08 -0400

    Update buyservers.js

--- scripts/buyservers.js
+++ scripts/buyservers.js
@@ -14,11 +14,9 @@ export async function main(ns) {
     }
     
     //if we have less than 25 owned servers, buy more
-    if (servers.length < maxServers) {
-      if (availableCash > ns.getPurchasedServerCost(16)) {
-        ns.tprint('Buying a server with 16 GB of RAM for ' + ns.getPurchasedServerCost(16));
-        ns.purchaseServer('pserv-' + servers.length, 16);
-      }
+    if (servers.length < maxServers && availableCash > ns.getPurchasedServerCost(16)) {
+      ns.tprint('Buying a server with 16 GB of RAM for ' + ns.getPurchasedServerCost(16));
+      ns.purchaseServer('pserv-' + servers.length, 16);
     }
 
     //check if pserv-(i) can be upgraded. if we can, and we can afford it, upgrade the ram by one level
@@ -27,7 +25,7 @@ export async function main(ns) {
         var servRam = ns.getServerMaxRam(servers[i]) * 2;
         ns.print(servRam)
         await ns.sleep(500)
-        if (ns.getServerMaxRam(servers[i]) < (2 ** 20) && availableCash >= ns.getPurchasedServerUpgradeCost(servers[i], servRam)) {
+        if (servRam < (2 ** 20) && availableCash >= ns.getPurchasedServerUpgradeCost('pserv-' + i, servRam)) {
           ns.upgradePurchasedServer(servers[i], (ns.getServerMaxRam(servers[i])) * 2);
           ns.tprint('Upgrading ' + servers[i] + ' to ' + ns.getServerMaxRam(servers[i]) + ' GB of RAM');
         } else {
@@ -36,6 +34,7 @@ export async function main(ns) {
               allMax = true;
             } else if (ns.getServerMaxRam(servers[(i)]) != (2 ** 20)) {
               allMax = false;
+              break;
             }
           }
           if (allMax == true) {
