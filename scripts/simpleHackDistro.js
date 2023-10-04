96052f668d0fddf9800211566ce66ffe0a333bb0
why-is-everything-taken <145640294+why-is-everything-taken@users.noreply.github.com>
2023-10-04 17:11:10 -0400

    Update simpleHackDistro.js
    Removing unnecessary comments and optimizing slightly

--- scripts/simpleHackDistro.js
+++ scripts/simpleHackDistro.js
@@ -1,54 +1,44 @@
 /** @param {NS} ns */
-
 import BaseServer from 'libs/serverinfo.js';
 import dpList from 'scripts/serverList.js';
 
 export async function main(ns) {
   let target = new BaseServer(ns, ns.args[0]);
-  ns.disableLog("ALL");
+  ns.disableLog('ALL');
   let sList = dpList(ns);
   let servers = [];
-  let server = new BaseServer(ns, s);
+  for (let s of sList) {
+    await ns.scp(['shared/wk.js', 'shared/gr.js', 'shared/hk.js'], s, 'home');
+    let server = new BaseServer(ns, s);
+    servers.push(server);
+  }
   while (true) {
-    sList = dpList(ns);
-    servers = [];
-    for (let s of sList) {
-      server = new BaseServer(ns, s);
-      servers.push(server);
-    }
-    //copies files
     for (let server of servers) {
-      if (ns.fileExists("shared/wk.js") == false) {
-        await ns.scp(["shared/wk.js", "shared/gr.js", "shared/hk.js"], server.id, "home");
-      }
-    }
-    for (let server of servers) {
+      //divert all threads to most valuable command
       if (server.admin && target.admin) {
-
-        // divert all threads to most valuable command
-        // weakens target if Security > Min Security
+        //weakens target if Security > Min Security
         if (target.security.level > target.security.min + 5) {
           let available_threads = server.threadCount(1.75);
           if (available_threads >= 1) {
-            ns.exec("shared/wk.js", server.id, available_threads, target.id);
+            ns.exec('shared/wk.js', server.id, available_threads, target.id);
           }
         }
-        // grows target if Money Available < Max Money
+        //grows target if Money Available < Max Money
         else if (target.money.available < target.money.max * .75) {
           let available_threads = server.threadCount(1.75);
           if (available_threads >= 1) {
-            ns.exec("shared/gr.js", server.id, available_threads, target.id);
+            ns.exec('shared/gr.js', server.id, available_threads, target.id);
           }
         }
-        // hack the target
+        //hack the target if previous conditions are met
         else {
           let available_threads = server.threadCount(1.7);
           if (available_threads >= 1) {
-            ns.exec("shared/hk.js", server.id, available_threads, target.id);
+            ns.exec('shared/hk.js', server.id, available_threads, target.id);
           }
         }
       }
-      //open ports
+      //open ports and nuke if we don't have root access
       else {
         try {
           ns.brutessh(server.id)
@@ -57,11 +47,8 @@ export async function main(ns) {
           ns.httpworm(server.id)
           ns.sqlinject(server.id)
         } catch { }
-
-        // try to nuke all servers
         try {
           ns.nuke(server.id);
-          //ns.singularity.installBackdoor(server.id);
         } catch { }
       }
     } await ns.sleep(100);
